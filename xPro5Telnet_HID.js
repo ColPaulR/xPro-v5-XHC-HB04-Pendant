706e86bf22642e3a6cf9e99a3c638b4c5caac81e
ColPaulR <35244547+ColPaulR@users.noreply.github.com>
2022-02-21 20:50:56 -0500

    update

--- xPro5Telnet_HID.js
+++ xPro5Telnet_HID.js
@@ -3,7 +3,7 @@ const {USB_Init, xhc_set_display } = require("./USB_devices");
 const { Telnet_Init, CNC_state } = require('./myTelnet');
 
 // Initialize Telnet
-const myTelnet = Telnet_Init(config,xhc_set_display);
+const myTelnet = Telnet_Init(config, xhc_set_display);
 
 // Get all settings
 myTelnet.write("$$\n");
